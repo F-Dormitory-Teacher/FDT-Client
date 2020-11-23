308230027afca2bc9bf1d66cf25ed10067b7d06d
윤석준 <tjrwns8024@naver.com>
2020-11-24 04:47:21 +0900

    [CREATE] <fragment/ NoticeDialoFragment.kt> create noticeDialogFragment.kt

---
+++ app/src/main/java/com/fdt/client/ui/dialog/NoticeDialogFragment.kt
@@ -0,0 +1,21 @@
+package com.fdt.client.ui.dialog
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.fdt.client.R
+
+class NoticeDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notice_dialog, container, false)
+    }
+
+}
