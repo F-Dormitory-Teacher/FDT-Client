4929c945f979d92f78b96fc88051536fd96794eb
윤석준 <tjrwns8024@naver.com>
2020-11-24 16:47:27 +0900

    [ADD] <fragment/ RequestFragment.kt>

--- app/src/main/java/com/fdt/client/ui/fragment/main/request/RequestFragment.kt
+++ app/src/main/java/com/fdt/client/ui/fragment/main/request/RequestFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.fdt.client.R
+import kotlinx.android.synthetic.main.fragment_request.*
 
 class RequestFragment : Fragment() {
 
@@ -16,4 +18,13 @@ class RequestFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_request, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        request_floating_btn.setOnClickListener {
+            Navigation.findNavController(requireActivity(),R.id.fragment_container).navigate(R.id.action_mainFragment_to_postRequestFragment)
+        }
+    }
+
 }
