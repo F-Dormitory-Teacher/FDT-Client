904fa8fb4205cf672eb1f34e33d3086164384570
윤석준 <tjrwns8024@naver.com>
2020-11-24 16:46:50 +0900

    [CREATE] <util/ FileUtil.kt>

---
+++ app/src/main/java/com/fdt/client/ui/util/FileUtil.kt
@@ -0,0 +1,30 @@
+package com.fdt.client.ui.util
+
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
+import androidx.annotation.RequiresApi
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
+
+
+object FileUtil {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    fun uriToFile(uri: Uri?, context: Context): String {
+        val cursor: Cursor = context.getContentResolver().query(uri!!, null, null, null)!!
+        cursor.moveToNext()
+        val path: String = cursor.getString(cursor.getColumnIndex("_data"))
+        cursor.close()
+        return path
+    }
+
+    fun createMultiPart(filePath: String?): MultipartBody.Part {
+        val file = File(filePath)
+        val requestBody: RequestBody = RequestBody.create("multipart/form-data".toMediaTypeOrNull(), file)
+        return MultipartBody.Part.createFormData("image", file.getName(), requestBody)
+    }
+}
