bea49f0312e06c37b417fbff3aea763282d68e35
윤석준 <tjrwns8024@naver.com>
2020-11-24 12:59:43 +0900

    [ADD] <remote/ NetRetrofit.kt> add OkHttpClient Builder

--- app/src/main/java/com/fdt/client/data/remote/NetRetrofit.kt
+++ app/src/main/java/com/fdt/client/data/remote/NetRetrofit.kt
@@ -1,18 +1,25 @@
 package com.fdt.client.data.remote
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object NetRetrofit {
+    val interceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
     var retrofit = Retrofit.Builder()
         .baseUrl("http://172.30.1.5:8080")
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    private var service: Api = retrofit.create(Api::class.java)
-
     fun getService(): Api? {
-        return service
+        return retrofit.create(Api::class.java)
     }
 }
