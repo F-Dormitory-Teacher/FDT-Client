0c8e27550db012b00e36f924be28e069c7ca46af
윤석준 <tjrwns8024@naver.com>
2020-11-24 12:58:27 +0900

    [MODIFY] <remote/ Api.kt> add @Header

--- app/src/main/java/com/fdt/client/data/remote/Api.kt
+++ app/src/main/java/com/fdt/client/data/remote/Api.kt
@@ -4,6 +4,7 @@ import com.fdt.client.entity.User
 import com.fdt.client.entity.response.Token
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface Api {
@@ -11,5 +12,5 @@ interface Api {
     fun postSignIn(@Body user: User) : Call<Token>
 
     @POST("/attend/createAttend")
-    fun postAttend() : Call<Void>
+    fun postAttend(@Header("Authorization") jwt: String) : Call<Void>
 }
