105bca907fe508f6d253639c5fb1a97da37882e6
윤석준 <tjrwns8024@naver.com>
2020-11-24 14:51:28 +0900

    [ADD] <remote/ Api.kt> add requestVerifyCode, confirmVerfiyCode, postSignUp

--- app/src/main/java/com/fdt/client/data/remote/Api.kt
+++ app/src/main/java/com/fdt/client/data/remote/Api.kt
@@ -1,16 +1,25 @@
 package com.fdt.client.data.remote
 
+import com.fdt.client.entity.Auth
+import com.fdt.client.entity.Email
 import com.fdt.client.entity.User
 import com.fdt.client.entity.response.Token
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface Api {
     @POST("/auth")
-    fun postSignIn(@Body user: User) : Call<Token>
+    fun postSignIn(@Body user: User): Call<Token>
 
     @POST("/attend/createAttend")
-    fun postAttend(@Header("Authorization") jwt: String) : Call<Void>
+    fun postAttend(@Header("Authorization") jwt: String): Call<Void>
+
+    @POST("/auth/auth-code")
+    fun requestVerifyCode(@Body email: Email): Call<Void>
+
+    @GET("/auth")
+    fun confirmVerifyCode(@Query("auth-code") code: String): Call<Void>
+
+    @POST("/auth/register")
+    fun postSignUp(@Body auth: Auth): Call<Void>
 }
