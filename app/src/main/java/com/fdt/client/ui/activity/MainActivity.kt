8f9aa18ca90f96885d49e7e75163b289b3e6d01d
윤석준 <tjrwns8024@naver.com>
2020-11-24 16:47:37 +0900

    .

--- app/src/main/java/com/fdt/client/ui/activity/MainActivity.kt
+++ app/src/main/java/com/fdt/client/ui/activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fdt.client.ui.activity
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -20,6 +22,7 @@ import splitties.toast.toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var sharedPref: SharedPref
+    lateinit var selectedImageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,42 +32,47 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val result: IntentResult =
-            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("requestCode", requestCode.toString())
+        if (requestCode == 65741) {
+            selectedImageUri = data?.data!!
+        } else {
+            val result: IntentResult =
+                IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
 
-        if (result == null) {
-            super.onActivityResult(requestCode, resultCode, data)
-        }
+            try {
+                //data를 jsonobject로 변경
+                val obj = JSONObject(result.contents)
 
-        try {
-            //data를 jsonobject로 변경
-            val obj = JSONObject(result.contents)
+                //qrcode가 있다면
+                if (result.contents != null && obj.getString("type") == "NIGHT" || obj.getString("type") == "MORNGING") {
+                    val response: Call<Void> =
+                        NetRetrofit.getService()!!.postAttend(sharedPref.getToken(true))
 
-            //qrcode가 있다면
-            if (result.contents != null && obj.getString("type") == "NIGHT" || obj.getString("type") == "MORNGING") {
-                val response: Call<Void> =
-                    NetRetrofit.getService()!!.postAttend(sharedPref.getToken(true))
-
-                response.enqueue(object : Callback<Void> {
-                    override fun onResponse(call: Call<Void>, response: Response<Void>) {
-                        if (response.code() == 200) {
-                            toast("출석 완료")
-                        } else if (response.code() == 409) {
-                            toast("이미 출석 체크한 유저입니다.")
+                    response.enqueue(object : Callback<Void> {
+                        override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                            if (response.code() == 200) {
+                                toast("출석 완료")
+                            } else if (response.code() == 409) {
+                                toast("이미 출석 체크한 유저입니다.")
+                            }
                         }
-                    }
 
-                    override fun onFailure(call: Call<Void>, t: Throwable) {
-                        Log.e("error", t.message.toString())
-                    }
-                })
-                return
-            } else Toast.makeText(this, "취소", Toast.LENGTH_SHORT).show()
+                        override fun onFailure(call: Call<Void>, t: Throwable) {
+                            Log.e("error", t.message.toString())
+                        }
+                    })
+                    return
+                } else Toast.makeText(this, "취소", Toast.LENGTH_SHORT).show()
 
-        } catch (e: JSONException) {
-            toast("유효하지 않은 qr code입니다.")
+            } catch (e: JSONException) {
+                toast("유효하지 않은 qr code입니다.")
+            }
         }
 
+    }
 
+    fun returnUri(): Uri {
+        return selectedImageUri
     }
 }
